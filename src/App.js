2df5e2cf55ff4a235f1204f7f0bbf1a43853aaa4
alexcamp140 <alexcamp140@gmail.com>
2023-01-13 14:48:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import {QRCodeSVG} from 'qrcode.react';
+
+
 
 function App() {
+
+  const [myInput, setMyInput]=useState(""); 
+
+  const [valueToManage, setValueToManage]=useState(""); 
+
+
+  function handleClick(){
+    setValueToManage(myInput);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <div className="card" style={{"width": "18rem"}}>
+      {valueToManage !=="" &&
+       <QRCodeSVG value={valueToManage} size="300"/>
+      }
+      
+        <div className="card-body">
+          <h5 className="card-title">QRcode generator</h5>
+          
+          <p className="card-text">Enter your code or your url to generate a QRCode.</p>
+          <input className='' type="text" id='inputValue' value={myInput} onChange={(e)=>setMyInput(e.target.value)}></input>
+              <button onClick={handleClick} className="btn btn-primary stretched-link"> Afficher QR code</button>
+        </div>
+      </div>
     </div>
   );
 }
